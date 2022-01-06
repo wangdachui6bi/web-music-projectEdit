f29d0664f72e8fd999385e677e379564040026fa
wk <wk@qq.com>
2022-01-06 22:21:20 +0800

    push第一次dev footer and artist

--- src/router/modules/DiscoverMusic.js
+++ src/router/modules/DiscoverMusic.js
@@ -3,6 +3,10 @@ export default [
     name: "DiscoverMusic",
     path: '/discovermusic',
     component: () => import('@/views/DiscoverMusic')
+  },
+  {
+    path: '/artistlist',
+    component: () => import('../../views/DiscoverMusic/ArtistsList.vue')
   }
 ]
 
