70ad2ebb0744d7978c1cfd3e0174052bb1b5939c
陈果 <2483545341@qq.com>
2022-01-06 22:01:13 +0800

    artist第一次push

--- src/router/modules/header.js
+++ src/router/modules/header.js
@@ -2,6 +2,10 @@ export default [
   {
     path: '/',
     component: () => import('@/commponents/header-bar/navlist.vue')
+  },
+  {
+    path: '/artistlist',
+    component: () => import('../../views/DiscoverMusic/ArtistsList.vue')
   }
 ]
 
