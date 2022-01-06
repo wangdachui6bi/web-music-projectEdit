940168eb195e6f52e930e318c961a1070b7ccf7a
王可 <2383998832@qq.com>
2022-01-06 06:48:06 +0000

    update vue.config.js.

--- vue.config.js
+++ vue.config.js
@@ -1,45 +1,45 @@
 module.exports = {
-  // 进行rem移动端 适配 
-  // css: {
-  //     loaderOptions: {
-  //         css: {},
-  //         postcss: {
-  //             plugins: [
-  //                 require('postcss-px2rem')({
-  //                     remUnit: 37.5
-  //                 })
-  //             ]
-  //         }
-  //     }
-  // },
-  // 开发者模式和生产模式不同接口
-  chainWebpack: config => {
-    // 发布模式
-    config.when(process.env.NODE_ENV === 'production', config => {
-      config
-        .entry('app')
-        .clear()
-        .add('./src/main-prod.js')
-      config
-        .set('externals', {
-          vant: 'vant',
-          vue: 'Vue',
-          'vue-router': 'VueRouter',
-          axios: 'axios',
-          nprogress: 'NProgress',
-          moment: 'moment'
+    // 进行rem移动端 适配 
+    // css: {
+    //     loaderOptions: {
+    //         css: {},
+    //         postcss: {
+    //             plugins: [
+    //                 require('postcss-px2rem')({
+    //                     remUnit: 37.5
+    //                 })
+    //             ]
+    //         }
+    //     }
+    // },
+    // 开发者模式和生产模式不同接口
+    chainWebpack: config => {
+        // 发布模式
+        config.when(process.env.NODE_ENV === 'production', config => {
+            config
+                .entry('app')
+                .clear()
+                .add('./src/main-prod.js')
+            config
+                .set('externals', {
+                    vant: 'vant',
+                    vue: 'Vue',
+                    'vue-router': 'VueRouter',
+                    axios: 'axios',
+                    nprogress: 'NProgress',
+                    moment: 'moment'
+                })
+            config.plugin('html').tap(args => {
+                args[0].isProd = true
+                return args
+            }) 
         })
-      config.plugin('html').tap(args => {
-        args[0].isProd = true
-        return args
-      })
-    })
-    // 开发模式
-    config.when(process.env.NODE_ENV === 'development', config => {
-      config
-        .entry('app')
-        .clear()
-        .add('./src/main-dev.js')
-    })
-  }
+        // 开发模式
+        config.when(process.env.NODE_ENV === 'development', config => {
+            config
+                .entry('app')
+                .clear()
+                .add('./src/main-dev.js')
+        })
+    }
 }
